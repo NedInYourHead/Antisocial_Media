55c2e5891f70beb77d85065c3cf82ce1b2693576
NedInYourHead <nedcorney@gmail.com>
2022-02-01 15:05:16 +0000

    Added Navmesh, Monster Follows Player

--- Antisocial Media/Assets/MonsterController.cs
+++ Antisocial Media/Assets/MonsterController.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterController : MonoBehaviour
 {
     public Transform player;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     void Update()
     {
         transform.LookAt(player);
+
+        agent.SetDestination(player.position);
     }
 }
