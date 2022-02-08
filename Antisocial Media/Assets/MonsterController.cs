3ce95fcbcd72e8f47bc3ab074181c4da9e5232e5
NedInYourHead <nedcorney@gmail.com>
2022-02-08 12:44:12 +0000

    Improved player chance of encountering (finished)

--- Antisocial Media/Assets/MonsterController.cs
+++ Antisocial Media/Assets/MonsterController.cs
@@ -13,6 +13,10 @@ public class MonsterController : MonoBehaviour
     [SerializeField] private float speedIncreaseDecrease = 0.66f;
     private float actualSpeedIncrease;
 
+    [SerializeField] private int centreCheckTime = 4;
+    [SerializeField] private float centreCheckDist = 4.0f;
+    private bool checkingDist = false; 
+
 
 
     private void Start()
@@ -22,6 +26,7 @@ public class MonsterController : MonoBehaviour
         centre = FindObjectOfType<CentrePriority>();
     }
 
+
     void Update()
     {
         transform.LookAt(player);
@@ -29,14 +34,29 @@ public class MonsterController : MonoBehaviour
         agent.SetDestination(player.position);
 
         Physics.Linecast(transform.position, player.position, out hit);
-        
-        if (hit.distance > 4 && centre.outOfSight) 
+
+        if ((hit.distance > centreCheckDist) && centre.outOfSight && !checkingDist) 
+        {
+            Debug.Log("checked distance1");
+            StartCoroutine(TeleToCentre());
+        }
+    }
+
+    IEnumerator TeleToCentre()
+    {
+        checkingDist = true;
+        yield return new WaitForSeconds(centreCheckTime);
+
+        if ((hit.distance > centreCheckDist) && centre.outOfSight)
         {
             agent.enabled = false;
             Debug.Log(centre.transform.position);
             transform.position = centre.transform.position;
             agent.enabled = true;
         }
+        Debug.Log("checked distance2");
+        Debug.Log(hit.distance);
+        checkingDist = false;
     }
 
     public void Dying()
@@ -53,5 +73,7 @@ public class MonsterController : MonoBehaviour
         speedIncrease = speedIncrease * speedIncreaseDecrease;
         actualSpeedIncrease = speedIncrease + 1.0f;
     }
+
+
 
 }
